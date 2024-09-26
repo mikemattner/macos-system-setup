27a8c8d1a4da2eae5ade8457fcda0815c7a995f2
Mike Mattner <mikemattner@gmail.com>
2024-09-26 11:15:50 -0400

    Update oh my zsh install path.

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,7 @@ function installBrews() {
 echo -n "Install Oh My Zsh? (y/n) "
 read REPLY
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi;
 
 echo -n "Need some brew? (y/n) "
