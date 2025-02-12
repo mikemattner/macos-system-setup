7a9c182689e30b1e8901af1ce6c4524c8181108e
Mike Mattner <mikemattner@Mikes-Work-MacBook-Pro.local>
2025-02-12 14:44:39 -0500

    Updating homebrew installation.

--- homebrew.sh
+++ homebrew.sh
@@ -22,6 +22,7 @@ brew tap homebrew/cask-fonts
 brew install --cask font-fira-code
 brew install --cask font-jetbrains-mono
 brew install --cask font-hack-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
 
 # Install alt-tab
 brew install --cask alt-tab
